24e93a99a309760d05031b61f0d76d979019e71e
shital-more <shital-more@users.noreply.github.com>
2016-05-17 11:50:20 +0530

    test

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ DB_con_ok=$(mysql -u root -p'swapnil123' -e "show databases;"|grep "mysql")
   if [[ $DB_con_ok != "mysql" ]]
      then
 	echo
-	echo "The DB connection could not be established. Check you username and password and try again."
+	echo "The DB connection check working could not be established. Check you username and password and try again."
 	echo
       else 
 	echo "Mysql Connection Ok"
